52dd8e2e2e86e6ddf1946122da8c1bc81ed3a216
Konstantin Gorodinskii <mail@konstantin.io>
2021-05-16 19:28:08 +0200

    Vector to HashMap
    makes more sense

--- 2020/day7/src/main.rs
+++ 2020/day7/src/main.rs
@@ -1,9 +1,9 @@
 use regex::Regex;
-use std::collections::HashSet;
-use std::hash::{ Hash, Hasher };
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs::read_to_string;
 use std::io;
+use std::ops::Deref;
 use std::str::FromStr;
 
 fn read_input() -> Result<String, io::Error> {
@@ -12,91 +12,69 @@ fn read_input() -> Result<String, io::Error> {
     Ok(result)
 }
 
-#[derive(Debug, Clone, Eq)]
-struct Bag {
-    name: String,
-    contains: Option<Vec<(u16, Bag)>>,
-}
+struct Bags(HashMap<String, Vec<(u8, String)>>);
 
-impl Hash for Bag {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.name.hash(state);
+impl Deref for Bags {
+    type Target = HashMap<String, Vec<(u8, String)>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
-impl PartialEq for Bag {
-    fn eq(&self, other: &Self) -> bool {
-        self.name == other.name
-    }
-}
-
-impl FromStr for Bag {
+impl FromStr for Bags {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let split = s.split(" bags contain ").collect::<Vec<&str>>();
+        let mut parsed = HashMap::new();
 
-        if let [name, contains_str] = &split[..] {
-            let mut contains: Vec<(u16, Bag)> = Vec::new();
+        for bag in s.lines() {
+            let split = bag.split(" bags contain ").collect::<Vec<&str>>();
 
-            for bag in contains_str.split(',') {
-                let re =
-                    Regex::new(r"(?P<can_contain>\d{1,})\s(?P<name>[a-z\s]*)\sbags?.?").unwrap();
+            if let [name, contains_str] = &split[..] {
+                let mut contains: Vec<(u8, String)> = Vec::new();
 
-                let captured = re.captures(bag);
+                for bag in contains_str.split(',') {
+                    let re = Regex::new(r"(?P<can_contain>\d{1,})\s(?P<name>[a-z\s]*)\sbags?.?")
+                        .unwrap();
 
-                if let Some(capture) = captured {
-                    let count = u16::from_str(&capture["can_contain"]).unwrap_or(0);
-                    let name = &capture["name"];
+                    let captured = re.captures(bag);
 
-                    contains.push((
-                        count,
-                        Bag {
-                            name: name.to_string(),
-                            contains: None,
-                        },
-                    ))
+                    if let Some(capture) = captured {
+                        let count = u8::from_str(&capture["can_contain"]).unwrap_or(0);
+                        let name = &capture["name"];
+
+                        contains.push((count, name.to_string()))
+                    }
                 }
-            }
 
-            return Ok(Bag {
-                name: name.to_string(),
-                contains: Some(contains),
-            });
+                parsed.insert(name.to_string(), contains);
+            }
         }
 
-        panic!("no bag")
+        Ok(Bags(parsed))
     }
 }
 
-fn count_bags(bag_name: &str, bags: &[Bag], except: &mut HashSet<Bag>) -> u16 {
-    let bags_count: Vec<Option<u16>> = bags
-        .iter()
-        .map(|bag| match &bag.contains {
-            Some(vec) => {
-                if except
-                    .iter()
-                    .any(|except_bag| except_bag.name == bag.name)
-                {
-                    return None;
-                }
+fn count_bags(bag_name: &str, bags: &Bags, except: &mut HashSet<String>) -> u16 {
+    let bags_count: Vec<Option<u16>> = bags.keys().map(|_bag_name| {
+        if except.contains(_bag_name) {
+            return None;
+        }
 
-                let found = vec
-                    .iter()
-                    .find(|(_, can_contain_bag)| can_contain_bag.name == *bag_name);
+        let found = bags[_bag_name]
+            .iter()
+            .find(|(_, can_contain_bag)| can_contain_bag == bag_name);
 
-                match found {
-                    Some((_, _)) => {
-                        except.insert(bag.clone());
+        match found {
+            Some((_, _)) => {
+                except.insert(_bag_name.clone());
 
-                        Some(1 + count_bags(&bag.name, bags, except))
-                    }
-                    _ => None,
-                }
+                Some(1 + count_bags(_bag_name.as_ref(), bags, except))
             }
             _ => None,
-        })
-        .collect();
+        }
+    }).collect();
 
     bags_count.iter().fold(0, |acc, count| match count {
         None => acc,
@@ -104,27 +82,20 @@ fn count_bags(bag_name: &str, bags: &[Bag], except: &mut HashSet<Bag>) -> u16 {
     })
 }
 
-fn count_containing(bag_name: &str, bags: &[Bag], initial_name: &str) -> u32 {
-    let bag = bags.iter().find(|b| b.name == bag_name).unwrap();
+fn count_containing(bag_name: &str, bags: &Bags, initial_name: &str) -> u32 {
+    let contains = bags[bag_name].clone();
 
-    if let Some(contains) = bag.clone().contains {
-        contains.iter().fold(0, |acc, (count, _bag)| {
-            let _count = count_containing(&_bag.name, bags, initial_name);
+    contains.iter().fold(0, |acc, (count, _bag_name)| {
+        let _count = count_containing(&_bag_name, bags, initial_name);
 
-            acc + u32::from(*count) + (u32::from(*count) * _count)
-        })
-    } else {
-        0
-    }
+        acc + u32::from(*count) + (u32::from(*count) * _count)
+    })
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input_data = read_input().unwrap();
 
-    let bags: Vec<Bag> = input_data
-        .lines()
-        .map(|bag_description| Bag::from_str(bag_description).unwrap())
-        .collect();
+    let bags = Bags::from_str(&input_data).unwrap();
 
     println!(
         "Part 1:\n {:#?}",
