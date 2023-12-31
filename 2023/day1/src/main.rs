d465beb79b05e7e6ed3483a0ff9fad563ae5cd06
Konstantin Gorodinskii <mail@konstantin.io>
2023-12-31 19:58:52 +1100

    one map

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -10,16 +10,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let sums: Vec<u32> = input
         .lines()
         .map(|line| {
-            line.chars()
-                .filter(|ch| ch.is_numeric())
-                .collect::<Vec<char>>()
+            let numeric_chars: Vec<char> = line.chars().filter(|ch| ch.is_numeric()).collect();
+            format!(
+                "{}{}",
+                numeric_chars[0],
+                numeric_chars[numeric_chars.len() - 1]
+            )
+            .parse::<u32>()
+            .unwrap_or(0)
         })
-        .map(|chars| {
-            let mut num = chars[0].to_string();
-            num.push(chars[chars.len() - 1]);
-            num.parse::<u32>().unwrap_or(0)
-        })
-        .collect(); //.map::u16(|num| num.parse::u16().unwrap_or(0))
+        .collect();
 
     println!("{:?}", sums.iter().sum::<u32>());
 
